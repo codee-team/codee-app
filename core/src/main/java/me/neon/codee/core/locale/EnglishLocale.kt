72965158db888fe3e05aff948614a071d6db5691
Neon <32961194+y9neon@users.noreply.github.com>
2021-06-01 23:35:36 +0300

    update: added new localized strings

--- core/src/main/java/me/neon/codee/core/locale/EnglishLocale.kt
+++ core/src/main/java/me/neon/codee/core/locale/EnglishLocale.kt
@@ -6,4 +6,10 @@ package me.neon.codee.core.locale
 object EnglishLocale : AbstractLocale() {
     override val languageCode: String = "en"
     override val titleCreateProject: String = "Create project"
+    override val titleNoProjects: String = "No projects!"
+    override val titleCreate: String = "Create"
+    override val titleProjectName: String = "Project name"
+    override val titleProjectType: String = "Project type"
+    override val titleTemplate: String = "Template"
+    override val titleTemplates: String = "Templates"
 }

63339d1f170df904a65538049056dbcc4156af09
Neon <32961194+y9neon@users.noreply.github.com>
2021-06-12 22:21:37 +0300

    update: kotlin version update `1.5.10` -> `1.5.20-M1`

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -2,5 +2,5 @@ object Version {
     const val COMPOSE = "1.0.0-beta08"
     const val APPLICATION_VERSION_INT = 1
     const val APPLICATION_VERSION_NAME = "1.0.1"
-    const val KOTLIN = "1.5.10"
+    const val KOTLIN = "1.5.20-M1"
 }

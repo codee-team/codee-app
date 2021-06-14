d20b9b045e7bdd3995f19f11d2f172bbdf05f014
Neon <32961194+y9neon@users.noreply.github.com>
2021-06-14 12:12:43 +0300

    style: code style fix

--- plugin-api/src/main/kotlin/com/codee/plugins/api/themes/ThemeColors.kt
+++ plugin-api/src/main/kotlin/com/codee/plugins/api/themes/ThemeColors.kt
@@ -5,14 +5,17 @@ public sealed interface ThemeColors {
      * Primary color of the app in HEX format.
      */
     public val primary: String
+
     /**
      * Secondary color of the app in HEX format.
      */
     public val secondary: String
+
     /**
      * Color onto primary color in HEX format.
      */
     public val onPrimary: String
+
     /**
      * Color onto secondary color in HEX format.
      */

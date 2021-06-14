a1558870a6545c37eeb0dce5d31933d57bb0f144
Neon <32961194+y9neon@users.noreply.github.com>
2021-06-14 20:07:21 +0300

    update: removed unnecessary permission

--- plugin-api/src/main/kotlin/com/codee/plugins/api/permissions/RemoveOtherThemesPermission.kt
+++
@@ -1,6 +0,0 @@
-package com.codee.plugins.api.permissions
-
-/**
- * Permission for removing other themes that registered by another plugins.
- */
-public object RemoveOtherThemesPermission : Permission

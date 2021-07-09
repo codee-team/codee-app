da478674785e3df1fb9c28ebe53a181611a7fa46
Neon <32961194+y9neon@users.noreply.github.com>
2021-07-09 23:09:44 +0300

    update: some ui

--- app/src/main/java/com/codee/app/AppActivity.kt
+++ app/src/main/java/com/codee/app/AppActivity.kt
@@ -3,12 +3,17 @@ package com.codee.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
+import com.codee.app.core.extensions.CodeeTheme
+import com.codee.app.screens.main.MainView
 
 class AppActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            CodeeTheme {
+                MainView(rememberNavController())
+            }
         }
     }
 }
--- app/src/main/java/com/codee/app/screens/projects/ProjectsView.kt
+++ app/src/main/java/com/codee/app/screens/projects/ProjectsView.kt
@@ -1,13 +1,21 @@
 package com.codee.app.screens.projects
 
+import androidx.compose.foundation.Image
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Create
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview
 @Composable
-fun ProjectsView() {
+fun ProjectsView() = Scaffold(
+    topBar = { Toolbar(title = "Codee") },
+    floatingActionButton = { FAB() }
+) {
 
 }
 
@@ -15,3 +23,14 @@ fun ProjectsView() {
 private fun Toolbar(title: String) = TopAppBar(
     title = { Text(text = title) }
 )
+
+@Composable
+private fun FAB() = FloatingActionButton(
+    onClick = {
+
+    }) {
+    Image(
+        imageVector = Icons.Outlined.Create,
+        contentDescription = null
+    )
+}

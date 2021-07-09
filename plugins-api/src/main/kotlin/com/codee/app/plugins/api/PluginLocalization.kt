669eca88ddc81e4965dbd530d031ceaae9b24a10
Neon <32961194+y9neon@users.noreply.github.com>
2021-07-09 20:11:05 +0300

    refactor: removed unnecessary `localizations`

--- plugins-api/src/main/kotlin/com/codee/app/plugins/api/PluginLocalization.kt
+++ plugins-api/src/main/kotlin/com/codee/app/plugins/api/PluginLocalization.kt
@@ -1,16 +1,10 @@
 package com.codee.app.plugins.api
 
 import com.codee.app.plugins.api.objects.CompatibilitySettings
-import com.codee.app.resources.locale.Locale
 
 public interface PluginLocalization {
     /**
      * Compatibility settings of plugin api.
      */
     public val compatibilitySettings: CompatibilitySettings
-
-    /**
-     * Localizations variants.
-     */
-    public val localizations: Set<Locale>
 }

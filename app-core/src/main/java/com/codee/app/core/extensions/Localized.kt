3c44e221f503ac58a15f7dec7d0fa2c6d485cc85
Alex Sokol <sokolsokolinskiy@gmail.com>
2021-07-14 21:10:20 +0300

    close #24: localize method improved
    Now the method follows [official](https://developer.android.com/guide/topics/resources/multilingual-support) documentations how to resolve resources

--- app-core/src/main/java/com/codee/app/core/extensions/Localized.kt
+++ app-core/src/main/java/com/codee/app/core/extensions/Localized.kt
@@ -2,21 +2,39 @@ package com.codee.app.core.extensions
 
 import androidx.core.os.ConfigurationCompat
 import com.codee.app.core.ContextDI.context
+import com.codee.app.resources.locale.Locale
 import com.codee.app.resources.locale.Localized
 import com.codee.app.resources.locale.get
 
+
+/**
+ * First it checks for full equality (languageCode and countryCode), then if there is no such a
+ * string, it searches for string with the same languageCode but without countryCode specified.
+ * Then the it tries to get just any string with the languageCode
+ *
+ * It repeats with all available locales and finally uses default variant as fallback
+ */
 fun <T> Localized<T>.localize(): T {
-    val locale = ConfigurationCompat
-        .getLocales(context.resources.configuration)[0]
-        .toCodeeLocale()
+    // getLocales returns custom class, not list
+    val locales = mutableListOf<Locale>().apply {
+        val list = ConfigurationCompat.getLocales(context.resources.configuration)
+        for(i in 1..list.size())
+            add(list[i].toCodeeLocale())
+    }
 
-    val variants = variants.toList()
+    for(locale in locales) {
+        val languageCodeMatches = variants
+            .filter { (k) -> k.languageCode == locale.languageCode }
+        val countryCodeMatches = languageCodeMatches
+            .filter { (k) -> k.countryCode == locale.countryCode }
 
-    val withLanguageCode = variants
-        .filter { (k) -> k.languageCode == locale.languageCode }
+        val result = countryCodeMatches.values.firstOrNull()
+            ?: languageCodeMatches.filter { (k) -> k.countryCode == null }.values.firstOrNull()
+            ?: languageCodeMatches.values.firstOrNull()
 
-    val withCountryCode = withLanguageCode
-        .firstOrNull { (k) -> k.countryCode == locale.countryCode }
+        if(result != null)
+            return result
+    }
 
-    return (withCountryCode ?: withLanguageCode.firstOrNull())?.second ?: default
+    return default
 }

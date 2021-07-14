9a7e371cd85a5c01953d580c197d4b975629f557
neon <justneon33@gmail.com>
2021-07-14 20:12:03 +0300

    style: `localize` style fix

--- app-core/src/main/java/com/codee/app/core/extensions/Localized.kt
+++ app-core/src/main/java/com/codee/app/core/extensions/Localized.kt
@@ -6,12 +6,17 @@ import com.codee.app.resources.locale.Localized
 import com.codee.app.resources.locale.get
 
 fun <T> Localized<T>.localize(): T {
-    val locale = ConfigurationCompat.getLocales(context.resources.configuration)[0]
+    val locale = ConfigurationCompat
+        .getLocales(context.resources.configuration)[0]
         .toCodeeLocale()
-    val vars = variants.toList()
-    return if(vars.any { it.first.languageCode == locale.languageCode }) {
-        if(vars.any { it.first.countryCode == locale.countryCode })
-            vars.first { it.first == locale }.second
-        vars.first { it.first.languageCode == locale.languageCode }.second
-    } else default
+
+    val variants = variants.toList()
+
+    val withLanguageCode = variants
+        .filter { (k) -> k.languageCode == locale.languageCode }
+
+    val withCountryCode = withLanguageCode
+        .firstOrNull { (k) -> k.countryCode == locale.countryCode }
+
+    return (withCountryCode ?: withLanguageCode.firstOrNull())?.second ?: default
 }

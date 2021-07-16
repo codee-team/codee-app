4da74567792e697299ede7c3d69848fffc8b444a
neon <justneon33@gmail.com>
2021-07-16 13:34:03 +0300

    feat: added implementations for `plugins-api`

--- app-core/src/main/java/com/codee/app/core/plugins/plugins.kt
+++ app-core/src/main/java/com/codee/app/core/plugins/plugins.kt
@@ -1,5 +1,6 @@
 package com.codee.app.core.plugins
 
+import com.codee.app.core.CodeeStrings
 import com.codee.app.core.delegates.oneTimeSet
 import com.codee.app.core.extensions.darkThemeColors
 import com.codee.app.core.extensions.lightThemeColors
@@ -7,8 +8,8 @@ import com.codee.app.core.extensions.toComposeColors
 import com.codee.app.plugins.api.Plugin
 import com.codee.app.plugins.api.PluginApi
 import com.codee.app.plugins.api.PluginLocalization
-import com.codee.app.plugins.api.container.AppContainer
-import com.codee.app.plugins.api.container.PluginApiManager
+import com.codee.app.plugins.api.container.*
+import com.codee.app.plugins.api.container.PluginApiContainer
 import com.codee.app.plugins.api.container.PluginLocalizationContainer
 import com.codee.app.plugins.api.container.ThemeContainer
 import com.codee.app.plugins.api.objects.PluginMetadata
@@ -16,15 +17,25 @@ import com.codee.app.resources.locale.Locale
 import com.codee.app.resources.theme.DarkThemeColors
 import com.codee.app.resources.theme.LightThemeColors
 import kotlinx.coroutines.flow.MutableSharedFlow
+import com.codee.app.plugins.api.container.ProjectTemplatesContainer
+import com.codee.app.resources.locale.strings.AppStrings
 
 class Plugin : Plugin {
     override var metadata: PluginMetadata by oneTimeSet()
     override val app: AppContainer = AppManager
-    override val apiManager: PluginApiManager = PluginApiManager
-    override val localizationContainer: PluginLocalizationContainer = PluginLocalizationContainer
+    override val apis: PluginApiContainer = PluginApiContainer
+    override val localizations: PluginLocalizationContainer = PluginLocalizationContainer
+    override val templates: ProjectTemplatesContainer = ProjectTemplatesContainer
 }
 
-private object PluginApiManager : PluginApiManager {
+private object ProjectTemplatesContainer : ProjectTemplatesContainer {
+    override val categories: MutableSharedFlow<ProjectsCategory> = MutableSharedFlow()
+    override fun <T : ProjectsCategory> register(instance: T) {
+        categories.tryEmit(instance)
+    }
+}
+
+private object PluginApiContainer : PluginApiContainer {
     override val apis: MutableSharedFlow<PluginApi> = MutableSharedFlow()
 
     override fun <T : PluginApi> register(instance: T): Boolean {
@@ -55,6 +66,7 @@ private object AppManager : AppContainer {
     override val versionName: String = "1.0"
     override val versionCode: Int = 1
     override val locale: Locale = Locale.en
+    override val strings: AppStrings = CodeeStrings
     override val themeContainer: ThemeContainer = ThemeContainer
 }
 

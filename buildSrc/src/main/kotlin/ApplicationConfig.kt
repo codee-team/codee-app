f59905a6038c67fd35f47127fe904bb02cd77d55
vadim <justneon33@gmail.com>
2021-07-27 17:10:25 +0300

    fix: temporary workaround due to [https://issuetracker.google.com/issues/194289155](this issue)

--- buildSrc/src/main/kotlin/ApplicationConfig.kt
+++ buildSrc/src/main/kotlin/ApplicationConfig.kt
@@ -6,7 +6,7 @@ object ApplicationConfig {
     const val APPLICATION_VERSION_INT = 1
     const val APPLICATION_VERSION_NAME = "1.0.1"
 
-    const val MIN_SDK_VERSION = 21
+    const val MIN_SDK_VERSION = 24 // temporary workaround due to error while desugarizing
     const val TARGET_SDK_VERSION = 30
     const val BUILD_TOOLS_VERSION = "30.0.3"
 }

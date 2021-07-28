a336a1416dadc525af7bbd495c8cf288285a1143
vadim <justneon33@gmail.com>
2021-07-29 00:29:45 +0300

    feat: `COMPOSE` updated to `1.0.0`

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -2,6 +2,6 @@ object Version {
     const val KOTLIN = "1.5.10"
 
     const val COROUTINES = "1.5.0"
-    const val COMPOSE = "1.0.0-rc02"
+    const val COMPOSE = "1.0.0"
     const val COMPOSE_ACCOMPANIST = "0.14.0"
 }
